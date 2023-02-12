9d6751390362da951a0dc6db11fb84e6ab98b1d6
yoon-b <eunbi.kim.kor@gmail.com>
2023-02-13 07:01:03 +0900

    problem solving 02

--- D4/5432_쇠막대기_자르기.py
+++ D4/5432_쇠막대기_자르기.py
@@ -2,17 +2,17 @@ T = int(input())
  
 for tc in range(1, T + 1):
     stick = input()
-    nStick = stick.replace('()', '#')
+    nStick = stick.replace('()', '#') # make lasers easier to identify
  
     stack, piece = 0, 0
  
     for i in nStick:
-        if i == '(':
+        if i == '(': # adding a new metal stick to the stack
             stack += 1
             piece += 1
         elif i == '#':
-            piece += stack
-        else:
+            piece += stack # a laser will add the stack number of pieces
+        else: # removing a metal stick from the stack
             stack -= 1
  
     print(f'#{tc} {piece}')
